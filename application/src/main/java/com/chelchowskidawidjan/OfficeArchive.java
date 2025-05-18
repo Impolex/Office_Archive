57211acfa762a15ccd89c6d9b48c0845ed72614d
Impolex <117767293+Impolex@users.noreply.github.com>
2025-05-18 22:52:53 +0200

    restructure

--- application/src/main/java/com/chelchowskidawidjan/OfficeArchive.java
+++ application/src/main/java/com/chelchowskidawidjan/OfficeArchive.java
@@ -1,6 +1,6 @@
 package com.chelchowskidawidjan;
 
-public class Main {
+public class OfficeArchive {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
