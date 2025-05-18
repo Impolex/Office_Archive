8552a842d6b7c42c22b7632a875f0fad22a02bc4
Impolex <117767293+Impolex@users.noreply.github.com>
2025-05-18 09:05:10 +0200

    fileService

---
+++ application/src/main/java/com/chelchowskidawidjan/FileService.java
@@ -0,0 +1,40 @@
+package com.chelchowskidawidjan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    private final FileRepository fileRepository;
+    @Autowired
+    public FileService(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    File getFileByUUID(UUID UUID){
+        return fileRepository.fetchFileByUUID(UUID);
+    }
+
+    boolean uploadFile(String fileName, User uploader, FileType fileType, byte[] fileContent) {
+        File file = new File(fileName, uploader, fileType, fileContent);
+        return fileRepository.persistFileUpload(file);
+    }
+
+    boolean editFile(File file, byte[] fileContent) {
+        file.modifyContent(fileContent);
+        return fileRepository.persistFileEdit(file);
+    }
+
+    boolean deleteFile(UUID UUID) {
+        return fileRepository.persistFileDeletion(UUID);
+    }
+
+    Map<String, UUID> getFilesForUser(UUID userUUID){
+        return fileRepository.fetchFilesForUser(userUUID);
+    }
+
+}
